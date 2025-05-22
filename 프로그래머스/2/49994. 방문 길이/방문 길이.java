c87ca7485e8c1bd0036b7f602c2982ec7f78af61
강명석 <dooly6276@gmail.com>
2025-05-22 13:10:53 +0900

    [level 2] Title: 방문 길이, Time: 17.45 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,50 +1,51 @@
 import java.util.*;
+
 class Solution {
-    private static final int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    private static boolean[][][] visited = new boolean[11][11][4];
-    private static class Node{
-        int r,c;
-        public Node(int r, int c){
-            this.r = r; this.c = c;
-        }
+    private static boolean isCanMove(int nx, int ny){
+        if (nx < 0 || nx > 10) {return false;}
+        if (ny < 0 || ny > 10) {return false;}
+        return true;
     }
-    private static Map<Character,Integer> map = new HashMap<>(){{
-    put('U',0); put('D',1); put('R',2); put('L',3);}};
+    
+    private static final HashMap<Character,int[]> move = new HashMap<>();
+    static{
+    move.put('U',new int[]{0,1});
+    move.put('D',new int[]{0,-1});
+    move.put('L',new int[]{-1,0});
+    move.put('R',new int[]{1,0});
+    }
+    
+    private static final HashSet<String> check = new HashSet<>();
     
     public int solution(String dirs) {
         int answer = 0;
-        Node cNode = new Node(5,5);
-        for (char cmd: dirs.toCharArray()){
-            int d = map.get(cmd);
-            Node pNode = new Node(cNode.r,cNode.c);
-            if (0 <= cNode.r + dir[d][0] && cNode.r + dir[d][0] <= 10
-               && 0 <= cNode.c + dir[d][1] && cNode.c + dir[d][1] <= 10){
-            cNode.r += dir[d][0]; cNode.c += dir[d][1];
-            if (visited[pNode.r][pNode.c][d] == false){
-                visited[pNode.r][pNode.c][d] = true;
-                visited[cNode.r][cNode.c][op(d)] = true;
-                answer += 1;
-                
-            }
-            }
-            }
-        return answer;
+        int x = 5, y = 5;
+        HashSet<String> ans = new HashSet<>();
+        
+        for (char c: dirs.toCharArray()){
+            int[] moveD = move.get(c);
+            int nx = moveD[0] + x, ny = moveD[1] + y;
+            if (!isCanMove(nx,ny)) {continue;}
+            check.add(nx+" "+ny+" "+x+" "+y);
+            check.add(x+" "+y+" "+nx+" "+ny);
+            x = nx;
+            y = ny;
         }
-    public int op(int cmd){
-        int tmp = 0;
-        switch(cmd){
-            case 0:
-                tmp = 1; break;
-            case 1:
-                tmp = 0; break;
-            case 2:
-                tmp = 3; break;
-            case 3:
-                tmp = 2; break;
-        }
-        return tmp;
+        
+        return check.size()/2;
+        
+        /*
+        길 하나씩
+            움직일 수 없으면
+                continue
+            움직일 수 있으면
+                움직인다
+                만약 움직인 곳이 새로우면 ans +1
+        */
+        
+        /*
+        
+        
+        */
     }
 }
-/*
-길 체크 방법 : 3차원 배열 [r][c][방향] 시작과 끝 둘 다 체크 필요 크기 11 시작 인덱스 [5][5]
-*/
