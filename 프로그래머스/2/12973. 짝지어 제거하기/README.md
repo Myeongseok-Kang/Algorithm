b99b98aad335e97d5e0b17c6e5434862053aac1b
강명석 <dooly6276@gmail.com>
2025-08-22 16:52:46 +0900

    [level 2] Title: 짝지어 제거하기, Time: 113.57 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 85.59 ms
+메모리: 12.5 MB, 시간: 113.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 16:22:21
+2025년 08월 22일 16:52:46
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,19 +1,24 @@
 def solution(s):
     answer = -1
+
+    """
+    모두제거 1
+    아니면 0
+    
+    for:  stack에 하나씩 순서대로 추가함
+    b     baa
+    """
     if len(s) == 1: return 0
     if len(s) == 2:
         if s[0] == s[1]: return 1
         else: return 0
-    """
     
-    
-    ab a
-    """
     stack = []
-    for c in s:
-        stack.append(c)
-        while len(stack) >= 2 and stack[-1] == stack[-2]:
+    stack.append(s[0])
+    for c in s[1:]:
+        if stack and stack[-1] == c:
             stack.pop()
-            stack.pop()            
+        else:
+            stack.append(c)
     if stack: return 0
     else: return 1
