5e29a706e11241045676385d13cfe3931e91c38e
강명석 <dooly6276@gmail.com>
2025-08-27 22:36:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.24 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,22 @@
 from collections import deque
 
-def solution(maps):
-    answer = -1
-    dir = [(-1,0),(1,0),(0,-1),(0,1)]
-    r,c = len(maps),len(maps[0])
-    visit = [[False for _ in range(c)] for _ in range(r)]
-    D = [[0 for _ in range(c)] for _ in range(r)]
-    D[0][0] = 1
+def bfs(m):
+    ly,lx = len(m),len(m[0])
+    dy = [-1,1,0,0]
+    dx = [0,0,-1,1]
+    q = deque()
+    q.append((0,0,1))
+    visit = [[False for _ in range(lx)] for _ in range(ly)]
     visit[0][0] = True
-    q = deque([(0,0)])
     while q:
-        cx,cy = q.popleft()
-        for dx,dy in dir:
-            nx,ny = cx+dx,cy+dy
-            if 0 <= nx <= r-1 and 0 <= ny <= c-1 and not visit[nx][ny] and maps[nx][ny]:
-                visit[nx][ny] = True
-                D[nx][ny] = D[cx][cy] + 1
-                q.append((nx,ny))
-    
-    if not visit[r-1][c-1]: return -1
-    else: return D[r-1][c-1]
-    """
-    1 0 1 1 1
-    1 0 1 0 1
-    1 0 1 1 1 
-    1 1 1 0 1
-    0 0 0 0 1
-    
-    """
+        cury,curx,curd = q.popleft()
+        for angle in range(4):
+            ny,nx,nd = cury + dy[angle] , curx + dx[angle],curd+1
+            if 0<=ny<=ly-1 and 0<=nx<=lx-1 and m[ny][nx] == 1 and not visit[ny][nx]:
+                if ny == ly-1 and nx == lx-1: return nd
+                visit[ny][nx] = True
+                q.append((ny,nx,nd))        
+    return -1
+
+def solution(maps):
+    return bfs(maps)
