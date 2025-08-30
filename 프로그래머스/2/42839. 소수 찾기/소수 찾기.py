3d169eddca29e0d4a7edfa77ed2dd9f16fdc768f
강명석 <dooly6276@gmail.com>
2025-08-30 23:46:31 +0900

    [level 2] Title: 소수 찾기, Time: 5.78 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,25 +1,24 @@
 from itertools import permutations
 
+def isprime(n):
+    if n == 0 or n == 1: return False
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
+
+nums = set()
+"""
+중복불가능, 앞에0붙으면같은거
+"""
 def solution(numbers):
-    numbers = [*map(str,numbers)]
-    answer = 0
+    numbers = list(numbers)
     visit = [False] * len(numbers)
     
-    
-    nums = set()
     for i in range(1,len(numbers)+1):
-        for num in permutations(numbers,i):
-            if num[0] != '0':
-                nums.add(int(''.join(num)))
-    nums = list(nums)
-    def isPrime(x):
-        if x == 1: return False
-        for i in range(2,int(x**0.5)+1):
-            if x % i == 0: return False
-        return True
-    ans = 0
-    for n in nums:
-        if isPrime(n): ans += 1
-        
-    
-    return ans
+        for j in permutations(numbers,i):
+            nums.add(int(''.join(j)))
+    ns = list(nums)
+    answer = 0
+    for n in ns:
+        if isprime(n): answer += 1
+    return answer
