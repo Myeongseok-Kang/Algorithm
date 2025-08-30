ad21beaaadff1496713950bf896e2b822c0ebe72
강명석 <dooly6276@gmail.com>
2025-08-30 22:19:28 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.44 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,12 +1,11 @@
-def solution(num):
-    if num == 1: return 0
+def f(num,depth):
+    if num == 1:
+        return depth
+    if depth == 500 and num != 1: return -1
+    if num % 2 == 0:
+        return f(num//2,depth+1)
+    else:
+        return f(num*3+1,depth+1)
 
-    def find(n,depth):
-        if n % 2 == 0: n //= 2
-        else:  n = (n*3+1)
-        
-        if n == 1: return depth
-        if depth == 500: return -1
-        return find(n,depth+1)
-    
-    return find(num,1)
+def solution(num):
+    return f(num,0)
