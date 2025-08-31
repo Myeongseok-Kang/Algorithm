e6afcacf122ae9814ead30dae1bf95a952c5f622
강명석 <dooly6276@gmail.com>
2025-08-31 21:31:43 +0900

    [level 3] Title: 불량 사용자, Time: 278.95 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,29 +1,25 @@
-import sys
-sys.setrecursionlimit(10**6)
+from itertools import permutations
+
+def match(s1,s2): #2번이 *들어감
+    if len(s1) != len(s2): return False
+    for i in range(len(s1)):
+        if s2[i] != '*' and s2[i] != s1[i]: return False
+    return True
 
 def solution(user_id, banned_id):
-    answer = 0
-    case = set() # 가능한 경우의 수: 넣을 때 튜플 변환 및 sort 함
+    memory = set()
+    for i in permutations(user_id,len(banned_id)):
+        i = list(i)
+        flag = True
+        for j in range(len(i)):
+            if not match(i[j],banned_id[j]): flag = False
+            
+        if flag == True:
+            k = sorted(i)
+            tmp = ''
+            for id in k:
+                tmp += id
+                tmp += ','
+            memory.add(tmp)
     
-    def compare(uid,bid):
-        if len(uid) != len(bid): return False
-        for i in range(len(uid)):
-            if not (bid[i] == '*' or bid[i] == uid[i]): return False
-        return True
-    
-    visit = [False] * len(user_id)
-    def dfs(cur,idx):
-        if idx == len(banned_id):
-            case.add(tuple(sorted(cur)))
-            return
-        for i in range(len(user_id)):
-            if compare(user_id[i],banned_id[idx]) and not visit[i]:
-                visit[i] = True
-                cur.append(i)
-                dfs(cur,idx+1)
-                cur.pop()
-                visit[i] = False
-    dfs([],0)
-        
-    
-    return len(case)
+    return len(memory)
