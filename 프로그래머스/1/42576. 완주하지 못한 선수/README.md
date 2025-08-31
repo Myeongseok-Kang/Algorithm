81b1b497e0e435a1d9ab037f52a161e7a12e3cdd
강명석 <dooly6276@gmail.com>
2025-09-01 00:58:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.99 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 95.9 MB, 시간: 85.54 ms
+메모리: 33.1 MB, 시간: 50.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 31일 23:35:44
+2025년 09월 01일 00:58:51
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,20 @@
 def solution(participant, completion):
-    count = dict()
+    dic = dict()
     for p in participant:
-        if p not in count:
-            count[p] = 1
+        if p in dic:
+            dic[p] += 1
         else:
-            count[p] += 1
+            dic[p] = 1
     for c in completion:
-        count[c] -= 1
+        if dic[c] > 1:
+            dic[c] -= 1
+        else:
+            del dic[c]
     
-    for key in count:
-        if count[key] == 1:
-            return key
-        
+    
     answer = ''
+    for key in dic:
+        answer = key
+    
+    
     return answer
