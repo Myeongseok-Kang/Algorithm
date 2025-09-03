368738667b3b268349a251c901e0245583f1d69b
강명석 <dooly6276@gmail.com>
2025-09-03 22:00:59 +0900

    [level 3] Title: 등굣길, Time: 26.93 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,22 +1,27 @@
 from collections import deque
+dy = [0,1]
+dx = [1,0]
 
 def solution(m, n, puddles):
-    dir = [(0,1),(1,0)]
-    answer = 0
-    r,c = n,m
-    A = [[0 for _ in range(c)] for _ in range(r)]
-    for tc,tr in puddles:
-        A[tr-1][tc-1] = -1
+    B = [[0] * m for _ in range(n)]
+    B[0][0] = 1
     
-    A[0][0] = 1
+    for x,y in puddles:
+        B[y-1][x-1] = -1
+    
     q = deque()
-    q.append((0,0))
+    q.append((0,0)) #y,x
     while q:
-        cx,cy = q.popleft()
-        for dx,dy in dir:
-            nx,ny = dx+cx,dy+cy
-            if 0 <= nx <= r-1 and 0 <= ny <= c-1 and A[nx][ny] != -1:
-                A[nx][ny] += A[cx][cy]
-                if (nx,ny) not in q: q.append((nx,ny))
+        cury,curx = q.popleft()
+        for i in range(2):
+            ny,nx = cury + dy[i] , curx +dx[i]
+            if 0<=ny<=n-1 and 0<=nx<=m-1 and B[ny][nx] != -1  :
+                if (ny,nx) not in q: q.append((ny,nx))
+                B[ny][nx] += B[cury][curx]
+                B[ny][nx] %= 1000000007
     
-    return A[r-1][c-1]%1000000007
+                    
+    return B[-1][-1]
+"""
+결과 나누기 주의
+"""
