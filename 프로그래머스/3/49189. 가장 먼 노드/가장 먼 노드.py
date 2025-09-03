f27bc3604d46660d7c2517609b1cd4c6b10bda3e
강명석 <dooly6276@gmail.com>
2025-09-03 23:02:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 42.29 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,22 +1,26 @@
 from collections import deque
 
 def solution(n, edge):
-    answer = 0
-    D = [-1] * n
-    D[0] = 0
+    V = [0] * n
     Elist = [[] for _ in range(n)]
-    for s,e in edge:
-        Elist[s-1].append(e-1)
+    for e,s in edge:
         Elist[e-1].append(s-1)
+        Elist[s-1].append(e-1)
     
-    q = deque([0])
+    V[0] = 0
+    q = deque()
+    q.append(0)
     while q:
         cur = q.popleft()
         for n in Elist[cur]:
-            if D[n] == -1:
-                D[n] = D[cur] + 1
+            if not V[n] and n != 0:
+                V[n] = V[cur] + 1
                 q.append(n)
-    m = max(D)
-    for i in D:
-        if i == m: answer += 1
-    return answer
+    max_val = max(V)
+    ans = V.count(max_val)
+    
+    """
+    0번 노드로부터 가장 멀리 떨어진 노드 개수
+    V가 0이면 시작이거나 아직 방문 안함 V에 거리 저장함
+    """
+    return ans
