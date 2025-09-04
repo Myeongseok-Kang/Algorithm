c83ca0c72d8d43dcf8c395a8ad01af24e0e1e49e
강명석 <dooly6276@gmail.com>
2025-09-04 23:56:46 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.33 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -2,30 +2,26 @@ from collections import deque
 dy = [-1,1,0,0]
 dx = [0,0,-1,1]
 
-
-def bfs(place):
-    for y in range(5):
-        for x in range(5):
-            if place[y][x] == 'P':
-                q = deque()
-                q.append((y,x,0))
-                visit = [[False] * 5 for _ in range(5)]
-                visit[y][x] = True
-                while q:    
-                    cury,curx,curd = q.popleft()
-                    if curd == 2: continue
-                    for angle in range(4):
-                        ny,nx,nd = cury+dy[angle],curx+dx[angle],curd+1
-                        if (0<=ny<=4 and 0<=nx<=4 and not visit[ny][nx] and place[ny][nx] != 'X'):
-                            if place[ny][nx] == 'P': return 0
-                            visit[ny][nx] = True
-                            q.append((ny,nx,nd))
-    return 1
-        
 def solution(places):
     
     answer = []
+    for place in places:
+        ans = 1
+        people = []
+        for y in range(5):
+            for x in range(5):
+                if place[y][x] == 'P': people.append((y,x))
+        
+        for y,x in people:
+            q = deque()
+            q.append((y,x,0))
+            while q:
+                cury,curx,curd = q.popleft()
+                for i in range(4):
+                    ny,nx = cury+dy[i],curx+dx[i]
+                    if 0<=ny<=4 and 0<=nx<=4 and place[ny][nx] != 'X' and curd <= 1:
+                        if place[ny][nx] == 'P' and not (ny == y and nx == x): ans = 0
+                        else: q.append((ny,nx,curd+1))
+        answer.append(ans)
 
-    for p in places:
-        answer.append(bfs(p))
     return answer
