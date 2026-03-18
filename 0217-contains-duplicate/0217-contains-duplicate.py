dcf34c28747da83cfb27b7132c559dafd90e2f7c
강명석 <dooly6276@gmail.com>
2026-03-18 19:18:57 +0900

    Time: 8 ms (78.76%), Space: 31.3 MB (66.43%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for i in nums:
+            if i not in s: s.add(i)
+            else: return True
+        return False
