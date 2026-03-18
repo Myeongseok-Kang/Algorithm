9e5be55108e2f27846d78498880ab80fad0b1b12
강명석 <dooly6276@gmail.com>
2026-03-18 17:40:04 +0900

    [level 2] Title: 삼각 달팽이, Time: 146.94 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,20 +1,34 @@
 def solution(n):
-    dy = [1,0,-1]
-    dx = [0,1,-1]
-    B = [[0] * n for _ in range(n)]
-    cy,cx = -1,0
-    angle = 0
-    num = 1
-    for i in range(n,0,-1): #4 3 2 1
+    dy,dx = (1,0,-1),(0,1,-1)
+    dd = 0
+    y,x = -1,0
+    tmp = 1
+    ar = []
+    for i in range(1,n+1):
+        ar.append([0 for _ in range(i)])
+    for i in range(n,0,-1):
         for _ in range(i):
-            ny,nx = cy+dy[angle],cx+dx[angle]
-            B[ny][nx] = num
-            num += 1
-            cy,cx = ny,nx
-        angle = (angle +1)%3
+            y,x = y+dy[dd],x+dx[dd]
+            ar[y][x] = tmp
+            tmp += 1
+        dd = (dd+1) % 3
     answer = []
+    for a in ar:
+        for n in a:
+            answer.append(n)
+        
     
-    for b in B:
-        for a in b:
-            if a != 0: answer.append(a)
+    
+    """
+    n=4
+    4 3 2 1
+    n = 5
+    5 4 3 2 1
+    
+    방향은 (+1,0)  (0,+1)  (-1,-1)   반복
+    처음 좌표를 (-1,0) 으로 ?
+    
+    
+    예외: n이 1 또는 2
+    """
     return answer
